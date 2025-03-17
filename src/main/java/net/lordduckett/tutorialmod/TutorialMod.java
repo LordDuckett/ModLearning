debeab41ef8e3e38e182fb14b2d9f556a4d35020
LordDuckett <duckett.ethan@gmail.com>
2025-03-17 00:41:00 -0700

    Changed Some Things

--- src/main/java/net/lordduckett/tutorialmod/TutorialMod.java
+++ src/main/java/net/lordduckett/tutorialmod/TutorialMod.java
@@ -1,5 +1,7 @@
 package net.lordduckett.tutorialmod;
 
+import net.lordduckett.tutorialmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -34,6 +36,8 @@ public class TutorialMod {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -46,6 +50,10 @@ public class TutorialMod {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.BISMUTH);
+            event.accept(ModItems.RAW_BISMUTH);
+        }
 
     }
 
